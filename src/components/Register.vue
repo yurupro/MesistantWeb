1d5cc09be474082cc1e12305238d96f5798e1cf4
Prokuma <prokuma@prokuma.kr>
2019-08-22 17:35:31 +0900

    登録フォームのアプデート

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,15 +4,36 @@
             <div class="col-lg-12">
                 <form>
                     <div class="form-group">
-                        <label for="registerEmailForm">Email</label>
+                        <label for="registerEmailForm">メールアドレス</label>
                         <input v-model="form_mail" type="email" class="form-control" id="registerEmailForm" placeholder="Email"/>
                     </div>
                     <div class="form-group">
-                        <label for="registerNameForm">Name</label>
+                        <label for="registerNameForm">氏名</label>
                         <input v-model="form_name" type="text" class="form-control" id="registerNameForm" placeholder="Name"/>
                     </div>
                     <div class="form-group">
-                        <label for="registerPasswordForm">Password</label>
+                        <label for="registerSexForm">性別</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="male"/>
+                            <label class="form-check-label" for="gridRadios1">
+                                男性
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="female"/>
+                            <label class="form-check-label" for="gridRadios1">
+                                女性
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="others"/>
+                            <label class="form-check-label" for="gridRadios1">
+                                その他
+                            </label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="registerPasswordForm">パスワード</label>
                         <input v-model="form_password" type="password" class="form-control" id="registerPasswordForm" placeholder="Password"/>
                     </div>
                     <router-link to="/"><button type="submit" class="btn btn-primary">戻る</button></router-link>
@@ -54,11 +75,12 @@ export default {
       this.$axios.post('http://koyume.prokuma.kr:8080/user/add', {'mail': this.form_mail, 'password': this.form_password, 'name': this.form_name, 'sex': this.form_sex})
         .then(response => {
           this.register_response = response
-          alert('登録成功')
+          alert('登録に成功しました！ログインしてください！')
           console.log(this.register_response)
+          this.$router.push({name: 'Login'})
         })
         .catch(error => {
-          alert('登録失敗')
+          alert('登録に失敗しました')
           console.log(error)
         })
     }
