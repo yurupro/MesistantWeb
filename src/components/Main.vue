c425f330f8bb1ca24185574f5cf726668d3be3db
Prokuma <prokuma@prokuma.kr>
2019-08-22 14:43:40 +0900

    フォーム実装（適当）

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,37 +2,54 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
+                <button  v-on:click="logout()" type="submit" class="btn btn-primary">ログアウト</button>
+            </div>
+        </div>
+
+        <div class="toast" role="alert" aria-live="assertive" aria-atomic="ture">
+            <div class="toast-header">
+                <strong class="mr-auto">ログイン</strong>
+                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="taost-body">
                 <form>
                     <div class="form-group">
-                        <label for="userIdForm">Email</label>
-                        <input v-model="mail" type="text" class="form-control" id="userIdForm" placeholder="Email"/>
+                        <label for="loginEmailForm">Email</label>
+                        <input v-model="form_mail" type="text" class="form-control" id="loginEmailForm" placeholder="Email"/>
                     </div>
                     <div class="form-group">
-                        <label for="passwordForm">Password</label>
-                        <input v-model="password" type="password" class="form-control" id="passwordForm" placeholder="Password"/>
+                        <label for="loginPasswordForm">Password</label>
+                        <input v-model="form_password" type="password" class="form-control" id="passwordForm" placeholder="Password"/>
                     </div>
-                    <button  v-on:click="login()" type="submit" class="btn btn-primary">Sign In</button>
+                    <button  v-on:click="login()" type="submit" class="btn btn-primary">ログイン</button>
                 </form>
             </div>
-            <div class="col-lg-12">
+        </div>
+
+        <div class="toast" role="alert" aria-live="assertive" aria-atomic="ture">
+            <div class="toast-header">
+                <strong class="mr-auto">ユーザ登録</strong>
+                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="taost-body">
                 <form>
                     <div class="form-group">
-                        <label for="userIdForm">Email</label>
-                        <input v-model="mail" type="text" class="form-control" id="userIdForm" placeholder="Email"/>
+                        <label for="registerEmailForm">Email</label>
+                        <input v-model="form_mail" type="email" class="form-control" id="registerEmailForm" placeholder="Email"/>
                     </div>
                     <div class="form-group">
-                        <label for="passwordForm">Password</label>
-                        <input v-model="password" type="password" class="form-control" id="passwordForm" placeholder="Password"/>
+                        <label for="registerNameForm">Name</label>
+                        <input v-model="form_name" type="text" class="form-control" id="registerNameForm" placeholder="Name"/>
                     </div>
                     <div class="form-group">
-                        <label for="nameForm">Password</label>
-                        <input v-model="password" type="password" class="form-control" id="passwordForm" placeholder="Password"/>
+                        <label for="registerPasswordForm">Password</label>
+                        <input v-model="form_password" type="password" class="form-control" id="registerPasswordForm" placeholder="Password"/>
                     </div>
-                    <div class="form-group">
-                        <label for="passwordForm">Password</label>
-                        <input v-model="password" type="password" class="form-control" id="passwordForm" placeholder="Password"/>
-                    </div>
-                    <button  v-on:click="register()" type="submit" class="btn btn-primary">Sign In</button>
+                    <button  v-on:click="register()" type="submit" class="btn btn-primary">登録</button>
                 </form>
             </div>
         </div>
@@ -44,21 +61,24 @@ export default {
   name: 'MesistantMain',
   data () {
     return {
-      mail: '',
-      password: '',
-      name: '',
-      sex: '',
+      form_mail: '',
+      form_password: '',
+      form_name: '',
+      form_sex: 'female',
       login_response: {},
       register_response: {}
     }
   },
   methods: {
     login: function () {
-      console.log(this.user)
-      this.$axios.post('http://koyume.prokuma.kr:8080/user/login', {headers: {'Content-Type': 'application/json'}}, {'mail': this.mail, 'password': this.password})
+      console.log(this.mail)
+      var data = {'mail': this.form_mail, 'password': this.form_password}
+      console.log(data)
+      this.$axios.post('http://koyume.prokuma.kr:8080/user/login', data)
         .then(response => {
           this.login_response = response
           console.log(this.login_response)
+          console.log('wei')
           this.$router.push({path: '/list', params: { user_id: this.login_response.id }})
         })
         .catch(error => {
@@ -66,9 +86,21 @@ export default {
           console.log(error, this.login_response)
         })
     },
+    logout: function () {
+      var data = {'mail': this.form_mail, 'password': this.form_password}
+      this.$axios.post('http://koyume.prokuma.kr:8080/user/logout', data)
+        .then(response => {
+          alert('ログアウト成功')
+          console.log(response)
+        })
+        .catch(error => {
+          alert('ログアウト失敗')
+          console.log(error, this.login_response)
+        })
+    },
     register: function () {
       console.log(this.user)
-      this.$axios.post('http://koyume.prokuma.kr:8080/user/add', {headers: {'Content-Type': 'application/json'}}, {'mail': this.mail, 'password': this.password, 'name': this.name, 'sex': this.sex})
+      this.$axios.post('http://koyume.prokuma.kr:8080/user/add', {'mail': this.form_mail, 'password': this.form_password, 'name': this.form_name, 'sex': this.form_sex})
         .then(response => {
           this.register_response = response
           alert('登録成功')
